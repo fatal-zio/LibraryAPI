aaaaed6c1325e1c813cb846b1d6ffb548009647d
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-09-28 10:13:05 -0500

    Completed initial version of Authors Controller.

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,28 @@
+﻿using LibraryAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorsController : Controller
+    {
+        private readonly ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
